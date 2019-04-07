501d0f9e58cd076d4fd7bc8a60af9bb1942ed759
Luca Hostettler <hostettler.luca1896@gmail.com>
2019-04-07 22:11:01 +0200

    Add Navigation Bar

---
+++ components/common/AppNavBar.tsx
@@ -0,0 +1,82 @@
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home'
+import { createStyles, WithStyles, withStyles } from '@material-ui/styles';
+import Link from 'next/link';
+import * as React from 'react';
+import getPageContext from '../../src/getPageContext';
+import { Auth0Authentication } from '../../src/utils/auth/Auth0Authentication';
+
+
+const styles = createStyles({
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+});
+
+
+export interface AppNavBarProps extends WithStyles<typeof styles> {
+  auth: Auth0Authentication;
+}
+class AppNavBar extends React.Component<AppNavBarProps>{
+
+  signInLink = React.forwardRef((props, ref) => <Link href="/auth/sign-in"><a>signin</a></Link>);
+  signOutLink = React.forwardRef((props, ref) => <Link href="/auth/sign-out"><a>signout</a></Link>);
+  profileLink = React.forwardRef((props, ref) => <Link href="/profile-class"><a>profile</a></Link>);
+  groupLink = React.forwardRef((props, ref) => <Link href="/group?title=Group12" as="g/Group12"><a>group</a></Link>);
+  aboutLink = React.forwardRef((props, ref) => <Link href="/about"><a>about</a></Link>);
+
+  render() {
+    const { classes } = this.props;
+    const authenticated = false;
+    return (
+      <div className={classes.root}> 
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu">
+              <HomeIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              color="inherit"
+              className={classes.grow}
+              style={{ textAlign: 'left' }}>
+              Biergit
+            </Typography>
+            {authenticated && (
+              <Button color="inherit" component={this.aboutLink}/>
+            )}
+            {authenticated && (
+              <Button color="inherit" component={this.groupLink}/>
+            )}
+            {authenticated && (
+              <Button color="inherit" component={this.profileLink}/>
+            )}
+            {!authenticated && (
+              <Button color="inherit" component={this.signInLink}/>
+            )}
+            {authenticated && (
+              <Button color="inherit" component={this.signOutLink}/>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+export default withStyles(styles as any, { withTheme: true })(AppNavBar as any);
